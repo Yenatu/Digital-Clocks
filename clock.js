6102f6c2eb5ebf1e91a7abd7e9811f55f350409a
yenatu <ayeneaddisie19@gmail.com>
2024-04-08 09:04:00 +0300

    digital clock

---
+++ clock.js
@@ -0,0 +1,14 @@
+function updateclock(){
+    const now=new Date();
+    let hour=now.getHours();
+    const meridiem=hour>=12?"PM":"AM";
+    hour=hour % 12 || 12;
+    hour=hour.toString().padStart(2,0);
+    const minute=now.getMinutes().toString().padStart(2,0);
+    const second=now.getSeconds().toString().padStart(2,0);
+    const timeString=`${hour}:${minute}:${second} ${meridiem}`;
+    document.getElementById("clock").textContent=timeString;
+}
+
+updateclock();
+setInterval(updateclock,1000);
